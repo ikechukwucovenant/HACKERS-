d94db7312f94418d2e30fe7caf0c58c6dcbfcf6f
ikechukwucovenant <covenantikechukwu062@gmail.com>
2026-05-12 11:34:39 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,20 @@
-hi
+let usedGuesses = [];
+
+// SMART COM GUESS SYSTEM
+function getAIGuess() {
+
+    let guess;
+
+    do {
+        guess = generateCode();
+    } while (usedGuesses.includes(guess));
+
+    usedGuesses.push(guess);
+
+    return guess;
+}
+
+// RESET AI MEMORY (called when game resets)
+function resetAI() {
+    usedGuesses = [];
+}
